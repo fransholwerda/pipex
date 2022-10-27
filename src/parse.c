4fd0c9eddbcb3ab3a8a9cc9e50888e7d91754c32
Frans Holwerda <fholwerd@f1r4s18.codam.nl>
2022-10-27 18:04:35 +0200

    parser stuff

--- src/parse.c
+++ src/parse.c
@@ -6,29 +6,45 @@
 /*   By: fholwerd <fholwerd@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/25 15:53:21 by fholwerd      #+#    #+#                 */
-/*   Updated: 2022/10/27 17:16:53 by fholwerd      ########   odam.nl         */
+/*   Updated: 2022/10/27 18:04:13 by fholwerd      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <struct_tools.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <get_paths.h>
 #include <utils.h>
 
-char	*check_cmd_path(char **paths, char *cmd)
+static char	*check_cmd_path(char **paths, char *cmd)
 {
-	
+	int		i;
+	char	*cmd_path;
+
+	i = 0;
+	while (paths[i])
+	{
+		cmd_path = ft_strjoin(paths[i], cmd);
+		if (access(cmd_path, F_OK))
+			return (cmd_path);
+		free(cmd_path);
+		i++;
+	}
+	return (NULL);
 }
 
-t_command	*parse_cmd(char **paths, char *arg)
+static t_command	*parse_cmd(char **paths, char *arg)
 {
-	char		*args;
-	char		*cmd;
-	t_command	*new_cmd;
+	char		**args;
+	char		*cmd_str;
+	char		*cmd_path;
+	t_command	*cmd;
 
 	args = ft_split(arg, ' ');
-	cmd = ft_strdup(args[0]);
-	
+	cmd_str = ft_strdup(args[0]);
+	cmd_path = check_cmd_path(paths, cmd_str);
+	cmd = new_cmd(cmd_path, cmd_str, args);
+	return (cmd);
 }
 
 t_command	*parse(int argc, char *argv[], char *env[])
@@ -41,6 +57,14 @@ t_command	*parse(int argc, char *argv[], char *env[])
 	paths = get_paths(env);
 	i = 1;
 	while (i < argc - 1)
+	{
+		if (!cmd)
+			cmd = parse_cmd(paths, argv[i]);
+		else
+			cmd_add_back(cmd, parse_cmd);
+	}
+	//REMOVE THIS
+	while (cmd)
 	{
 		
 	}
